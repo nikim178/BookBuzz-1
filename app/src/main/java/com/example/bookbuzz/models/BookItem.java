6f4d866869153ff22a90f4b1837cb39b69a1df0b
srushti patil <nirisrusha4@gmail.com>
2020-12-03 18:03:45 +0530

    new search activity added

---
+++ app/src/main/java/com/example/bookbuzz/models/BookItem.java
@@ -0,0 +1,23 @@
+package com.example.bookbuzz.models;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BookItem {
+    public String id;
+    public String title;
+    public List<String> authors = new ArrayList<>();
+    public String description;
+    public String imageLink;
+
+    public String getAuthorsString() {
+        String text = "";
+        if (authors.size() == 0) {
+            return "";
+        }
+        for (String author : authors) {
+            text = text + author + ", ";
+        }
+        return text.substring(0, text.length() - 2);
+    }
+}
