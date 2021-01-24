e9f38002efda20d063402684d9de3389dba0221e
srushti patil <nirisrusha4@gmail.com>
2021-01-24 21:01:58 +0530

    SwipeGenre activity completed and google authentication removed

--- app/src/main/java/com/example/bookbuzz/SwipeGenre.java
+++ app/src/main/java/com/example/bookbuzz/SwipeGenre.java
@@ -112,67 +112,7 @@ public class SwipeGenre extends AppCompatActivity {
     private List<ItemModel> addList() {
         List<ItemModel> items = new ArrayList<>();
         storeResult = (ArrayList<String>) getIntent().getSerializableExtra("book");
-        // Single Button
-        /* (storeResult.contains("NonFiction")) {
-            items.add(new ItemModel(R.drawable.nonfiction, "Sapiens"));
-            items.add(new ItemModel(R.drawable.nonfiction2, "A Brief History Of Time"));
-            items.add(new ItemModel(R.drawable.nonfiction3, "I Know Why The Caged Bird Sings"));
-            items.add(new ItemModel(R.drawable.nonfiction4, "Into Thin Air"));
-            items.add(new ItemModel(R.drawable.nonfiction5, "Bad Blood"));
-            items.add(new ItemModel(R.drawable.nonfiction6, "The Diary Of Young Girl"));
-            items.add(new ItemModel(R.drawable.nonfiction7, "Man's Search For Meaning"));
-            return items;
-        }*/
-       /*else if(storeResult.contains("Romance")) {
-            items.add(new ItemModel(R.drawable.romance, "The Notebook"));
-            items.add(new ItemModel(R.drawable.romance2, "The Fault in Our Stars"));
-            items.add(new ItemModel(R.drawable.romance3, "Gone with the Wind"));
-            items.add(new ItemModel(R.drawable.romance4, "Beautiful Disaster"));
-            items.add(new ItemModel(R.drawable.romance5, "Outlander"));
-            items.add(new ItemModel(R.drawable.romance6, "Jane Eyre"));
-            items.add(new ItemModel(R.drawable.romance7, "A Long Petal of the Sea"));
-            return items;
-        }
-        else if(storeResult.contains("Fantasy")) {
-            items.add(new ItemModel(R.drawable.fantasy, "Harry Potter"));
-            items.add(new ItemModel(R.drawable.fantasy2, "The Hobbit"));
-            items.add(new ItemModel(R.drawable.fantasy3, "A Game of Thrones"));
-            items.add(new ItemModel(R.drawable.fantasy4, "The Palace Of Illusions"));
-            items.add(new ItemModel(R.drawable.fantasy5, "Malgudi Day's"));
-            items.add(new ItemModel(R.drawable.fantasy6, "Alice's Adventures in Wonderland"));
-            items.add(new ItemModel(R.drawable.fantasy7, "The Time Machine"));
-            return items;
-        }
-        else if(storeResult.contains("SelfHelp")){
-            items.add(new ItemModel(R.drawable.selfhelp, "The 7 Habits"));
-            items.add(new ItemModel(R.drawable.selfhelp2, "How to Win Friends"));
-            items.add(new ItemModel(R.drawable.selfhelp3, "Think and Grow Rich"));
-            items.add(new ItemModel(R.drawable.selfhelp4, "The 4-Hour Work Week"));
-            items.add(new ItemModel(R.drawable.selfhelp5, "The Secret"));
-            items.add(new ItemModel(R.drawable.selfhelp6, "Rich Dad Poor Dad"));
-            items.add(new ItemModel(R.drawable.selfhelp7, "Atomic Habits"));
-            return items;
-        }
-        else if(storeResult.contains("Detective")){
-            items.add(new ItemModel(R.drawable.detective, "Murder on the Orient Express"));
-            items.add(new ItemModel(R.drawable.detective1, "Sherlock Holmes"));
-            items.add(new ItemModel(R.drawable.detective2, "The Girl with the Dragon Tatoo"));
-            items.add(new ItemModel(R.drawable.detective3, "The murder of Roger Ackroyd"));
-            items.add(new ItemModel(R.drawable.detective4, "The Murders in the Rue Morgue"));
-            items.add(new ItemModel(R.drawable.detective5, "The Guest List"));
-            items.add(new ItemModel(R.drawable.detective7, "A Study in Scarlet"));
-            return items;
-        }
-        else if(storeResult.contains("Comic")) {
-            items.add(new ItemModel(R.drawable.comic, "Nimona"));
-            items.add(new ItemModel(R.drawable.comic2, "Justic League"));
-            items.add(new ItemModel(R.drawable.comic3, "Batman"));
-            items.add(new ItemModel(R.drawable.comic4, "Daytripper"));
-            items.add(new ItemModel(R.drawable.comic5, "Once & Future"));
-            items.add(new ItemModel(R.drawable.comic6, "The Sherif Of Babylon"));
-            items.add(new ItemModel(R.drawable.comic7, "Iron Man"));
-            return items;
-        }*/
+
         // Any one with nonfiction
        if(storeResult.contains("NonFiction") && storeResult.contains("Romance"))
         {
@@ -439,6 +379,66 @@ public class SwipeGenre extends AppCompatActivity {
             items.add(new ItemModel(R.drawable.comic7, "Iron Man"));
             return items;
 
+        }
+        //single options
+        else if(storeResult.contains("NonFiction")) {
+            items.add(new ItemModel(R.drawable.nonfiction, "Sapiens"));
+            items.add(new ItemModel(R.drawable.nonfiction2, "A Brief History Of Time"));
+            items.add(new ItemModel(R.drawable.nonfiction3, "I Know Why The Caged Bird Sings"));
+            items.add(new ItemModel(R.drawable.nonfiction4, "Into Thin Air"));
+            items.add(new ItemModel(R.drawable.nonfiction5, "Bad Blood"));
+            items.add(new ItemModel(R.drawable.nonfiction6, "The Diary Of Young Girl"));
+            items.add(new ItemModel(R.drawable.nonfiction7, "Man's Search For Meaning"));
+            return items;
+        }else if(storeResult.contains("Romance")) {
+            items.add(new ItemModel(R.drawable.romance, "The Notebook"));
+            items.add(new ItemModel(R.drawable.romance2, "The Fault in Our Stars"));
+            items.add(new ItemModel(R.drawable.romance3, "Gone with the Wind"));
+            items.add(new ItemModel(R.drawable.romance4, "Beautiful Disaster"));
+            items.add(new ItemModel(R.drawable.romance5, "Outlander"));
+            items.add(new ItemModel(R.drawable.romance6, "Jane Eyre"));
+            items.add(new ItemModel(R.drawable.romance7, "A Long Petal of the Sea"));
+            return items;
+        }
+        else if(storeResult.contains("Fantasy")) {
+            items.add(new ItemModel(R.drawable.fantasy, "Harry Potter"));
+            items.add(new ItemModel(R.drawable.fantasy2, "The Hobbit"));
+            items.add(new ItemModel(R.drawable.fantasy3, "A Game of Thrones"));
+            items.add(new ItemModel(R.drawable.fantasy4, "The Palace Of Illusions"));
+            items.add(new ItemModel(R.drawable.fantasy5, "Malgudi Day's"));
+            items.add(new ItemModel(R.drawable.fantasy6, "Alice's Adventures in Wonderland"));
+            items.add(new ItemModel(R.drawable.fantasy7, "The Time Machine"));
+            return items;
+        }
+        else if(storeResult.contains("SelfHelp")){
+            items.add(new ItemModel(R.drawable.selfhelp, "The 7 Habits"));
+            items.add(new ItemModel(R.drawable.selfhelp2, "How to Win Friends"));
+            items.add(new ItemModel(R.drawable.selfhelp3, "Think and Grow Rich"));
+            items.add(new ItemModel(R.drawable.selfhelp4, "The 4-Hour Work Week"));
+            items.add(new ItemModel(R.drawable.selfhelp5, "The Secret"));
+            items.add(new ItemModel(R.drawable.selfhelp6, "Rich Dad Poor Dad"));
+            items.add(new ItemModel(R.drawable.selfhelp7, "Atomic Habits"));
+            return items;
+        }
+        else if(storeResult.contains("Detective")){
+            items.add(new ItemModel(R.drawable.detective, "Murder on the Orient Express"));
+            items.add(new ItemModel(R.drawable.detective1, "Sherlock Holmes"));
+            items.add(new ItemModel(R.drawable.detective2, "The Girl with the Dragon Tatoo"));
+            items.add(new ItemModel(R.drawable.detective3, "The murder of Roger Ackroyd"));
+            items.add(new ItemModel(R.drawable.detective4, "The Murders in the Rue Morgue"));
+            items.add(new ItemModel(R.drawable.detective5, "The Guest List"));
+            items.add(new ItemModel(R.drawable.detective7, "A Study in Scarlet"));
+            return items;
+        }
+        else if(storeResult.contains("Comic")) {
+            items.add(new ItemModel(R.drawable.comic, "Nimona"));
+            items.add(new ItemModel(R.drawable.comic2, "Justic League"));
+            items.add(new ItemModel(R.drawable.comic3, "Batman"));
+            items.add(new ItemModel(R.drawable.comic4, "Daytripper"));
+            items.add(new ItemModel(R.drawable.comic5, "Once & Future"));
+            items.add(new ItemModel(R.drawable.comic6, "The Sherif Of Babylon"));
+            items.add(new ItemModel(R.drawable.comic7, "Iron Man"));
+            return items;
         }
         else
         {
