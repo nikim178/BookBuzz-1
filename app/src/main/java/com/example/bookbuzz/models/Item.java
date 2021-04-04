4338cfcf1f11f5bac64cacc15ece32d7f123e144
srushti patil <nirisrusha4@gmail.com>
2021-04-05 00:00:54 +0530

    swipe works fine

---
+++ app/src/main/java/com/example/bookbuzz/models/Item.java
@@ -0,0 +1,10 @@
+package com.example.bookbuzz.models;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Item {
+        public String title;
+        public String imageLink;
+}
+
